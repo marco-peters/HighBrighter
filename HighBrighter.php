4be2ba71ee8981d0557c776e97495a5d76b45e3a
Marco Peters <marcopeters@MacBook-Pro-van-Marco.local>
2015-01-24 13:23:09 +0100

    Start with main HighBrighter class

---
+++ HighBrighter.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This is the main loading file for HighBrighter
+ *
+ * PHP version > 5.5
+ *
+ * @package     HighBrighter
+ * @author      Marco Peters <marcopeters16@gmail.com>
+ * @copyright   2015 Marco Peters
+ * @link        https://github.com/marco-peters/HighBrighter
+ * @since       File available since Release 0.1
+ */
+
+/**
+ * The main HighBrighter class
+ *
+ * @package    HighBrighter
+ * @author      Marco Peters <marcopeters16@gmail.com>
+ * @copyright   2015 Marco Peters
+ * @link        https://github.com/marco-peters/HighBrighter
+ * @since       File available since Release 0.1
+ */
+class HighBrighter
+{
+}
